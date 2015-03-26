585d7bab6dfefd163f1985fc37f2338905bdeaef
mobilitips <mobilitips@hotmail.com>
2015-03-26 09:53:14 +0100

    [MvxAms - Core] Api service added

--- MobiliTips.MvxPlugin.MvxAms/MobiliTips.MvxPlugin.MvxAms/Api/MvxAmsApiService.cs
+++ MobiliTips.MvxPlugin.MvxAms/MobiliTips.MvxPlugin.MvxAms/Api/MvxAmsApiService.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Cirrious.CrossCore;
+using Cirrious.MvvmCross.Plugins.Messenger;
 using Microsoft.WindowsAzure.MobileServices;
 
 namespace MobiliTips.MvxPlugin.MvxAms.Api
@@ -8,36 +10,78 @@ namespace MobiliTips.MvxPlugin.MvxAms.Api
     public class MvxAmsApiService : IMvxAmsApiService
     {
         private readonly MobileServiceClient _client;
+        private readonly IMvxMessenger _messenger;
 
         public MvxAmsApiService(MobileServiceClient client)
         {
             _client = client;
+            _messenger = Mvx.Resolve<IMvxMessenger>();
         }
 
-        public Task<T> InvokeApiAsync<T>(string apiName)
+        public async Task<T> InvokeApiAsync<T>(string apiName)
         {
-            throw new System.NotImplementedException();
+            try
+            {
+                return await _client.InvokeApiAsync<T>(apiName);
+            }
+            catch (MobileServiceInvalidOperationException ex)
+            {
+                _messenger.Publish(new MvxAmsErrorMessage(this, ex));
+                return default(T);
+            }
         }
 
-        public Task<U> InvokeApiAsync<T, U>(string apiName, T body)
+        public async Task<U> InvokeApiAsync<T, U>(string apiName, T body)
         {
-            throw new System.NotImplementedException();
+            try
+            {
+                return await _client.InvokeApiAsync<T, U>(apiName, body);
+            }
+            catch (MobileServiceInvalidOperationException ex)
+            {
+                _messenger.Publish(new MvxAmsErrorMessage(this, ex));
+                return default(U);
+            }
         }
 
-        public Task<T> InvokeApiAsync<T>(string apiName, HttpMethod method, IDictionary<string, string> parameters)
+        public async Task<T> InvokeApiAsync<T>(string apiName, HttpMethod method, IDictionary<string, string> parameters)
         {
-            throw new System.NotImplementedException();
+            try
+            {
+                return await _client.InvokeApiAsync<T>(apiName, method, parameters);
+            }
+            catch (MobileServiceInvalidOperationException ex)
+            {
+                _messenger.Publish(new MvxAmsErrorMessage(this, ex));
+                return default(T);
+            }
         }
 
-        public Task<U> InvokeApiAsync<T, U>(string apiName, T body, HttpMethod method, IDictionary<string, string> parameters)
+        public async Task<U> InvokeApiAsync<T, U>(string apiName, T body, HttpMethod method, IDictionary<string, string> parameters)
         {
-            throw new System.NotImplementedException();
+            try
+            {
+                return await _client.InvokeApiAsync<T, U>(apiName, body, method, parameters);
+            }
+            catch (MobileServiceInvalidOperationException ex)
+            {
+                _messenger.Publish(new MvxAmsErrorMessage(this, ex));
+                return default(U);
+            }
         }
 
-        public Task<HttpResponseMessage> InvokeApiAsync(string apiName, HttpContent content, HttpMethod method, IDictionary<string, string> requestHeaders,
+        public async Task<HttpResponseMessage> InvokeApiAsync(string apiName, HttpContent content, HttpMethod method, IDictionary<string, string> requestHeaders,
             IDictionary<string, string> parameters)
         {
-            throw new System.NotImplementedException();
+            try
+            {
+                return await _client.InvokeApiAsync(apiName, content, method, requestHeaders, parameters);
+            }
+            catch (MobileServiceInvalidOperationException ex)
+            {
+                _messenger.Publish(new MvxAmsErrorMessage(this, ex));
+                return null;
+            }
         }
     }
 }
