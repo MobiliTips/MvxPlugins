5f3be30f18b4b5a5f9ce32852677f1f3ef0402b1
mobilitips <mobilitips@hotmail.com>
2015-03-30 15:08:29 +0200

    [MvxForms - All] Preparing solution for WindowsCommon

---
+++ MobiliTips.MvxPlugin.MvxForms/Samples/MobiliTips.MvxPlugin.MvxForms.Sample.WinPhone/Views/FirstView.xaml.cs
@@ -0,0 +1,12 @@
+using Cirrious.MvvmCross.WindowsPhone.Views;
+
+namespace MobiliTips.MvxPlugin.MvxForms.Sample.WinPhone.Views
+{
+    public partial class FirstView : MvxPhonePage
+    {
+        public FirstView()
+        {
+            InitializeComponent();
+        }
+    }
+}
