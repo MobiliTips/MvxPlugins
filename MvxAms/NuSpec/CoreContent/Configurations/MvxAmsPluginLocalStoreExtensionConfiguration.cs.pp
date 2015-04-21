7dc08a52aa104fc08078eb9ac111a929c5bc9598
mobilitips <mobilitips@hotmail.com>
2015-04-21 18:37:33 +0200

    [MvxAms] LocalStore extension NuSpec updated

---
+++ MvxAms/NuSpec/CoreContent/Configurations/MvxAmsPluginLocalStoreExtensionConfiguration.cs.pp
@@ -0,0 +1,14 @@
+﻿using MobiliTips.MvxPlugins.MvxAms.LocalStore;
+
+namespace $rootnamespace$.Configurations
+{
+    public class MvxAmsPluginLocalStoreExtensionConfiguration : MvxAmsPluginLocalStoreExtensionBaseConfiguration
+    {
+        public MvxAmsPluginLocalStoreExtensionConfiguration(string databaseFullPath) : base(databaseFullPath)
+        {
+            // Uncomment if you want to provide for all platforms
+            // another database file name than the default amslocalstore.db one
+            //DatabaseFileName = "YOUR_CUSTOM_NAME_WITH_DB_EXTENSION";
+        }
+    }
+}
